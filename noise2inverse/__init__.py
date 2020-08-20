31f5fc4f01dd06c008c076a9ff9396aab8d4abdf
Allard Hendriksen <allard.hendriksen@cwi.nl>
2020-08-20 10:45:11 +0200

    Make package installable from pip+git
    This should now work:
    pip install git+https://github.com/ahendriksen/noise2inverse.git

---
+++ noise2inverse/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for noise2inverse."""
+
+__author__ = """Allard Hendriksen"""
+__email__ = "allard.hendriksen@cwi.nl"
+
+
+def __get_version():
+    import os.path
+
+    version_filename = os.path.join(os.path.dirname(__file__), "VERSION")
+    with open(version_filename) as version_file:
+        version = version_file.read().strip()
+    return version
+
+
+__version__ = __get_version()
+
+from . import datasets
+from . import fig
+from . import noise
+from . import tiffs
+from . import tomo
--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     entry_points='''
         [console_scripts]
     ''',
-    packages=find_packages(),
+    packages=find_packages(include=['noise2inverse']),
     package_dir={'noise2inverse': 'noise2inverse'},
     setup_requires=setup_requirements,
     test_suite='tests',
